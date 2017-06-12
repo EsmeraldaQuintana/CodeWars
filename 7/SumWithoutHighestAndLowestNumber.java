773fee6aca43279410924afbc1576e1df7704717
Esmeralda Quintana <quintaen195@potsdam.edu>
2017-06-12 14:00:30 -0400

    start IntStream solution...

--- 7/SumWithoutHighestAndLowestNumber.java
+++ 7/SumWithoutHighestAndLowestNumber.java
@@ -1,15 +1,20 @@
 import java.util.Arrays;
 
+import java.util.stream.IntStream;
+
 public class SumWithoutHighestAndLowestNumber {
 
-        static int ARRAY_LENGTH = 5;
+        static int ARRAY_LENGTH = 3;
+        static int SOL_TEST = 1;
 
         public static void main(String[] args) {
             int[] testArray = new int[ARRAY_LENGTH];
             for (int i = 0; i < ARRAY_LENGTH; i++)
                 testArray[i] = (int) Math.floor(Math.random()*100 - 50);
             System.out.println("Random array: " + Arrays.toString(testArray));
-            int sum_test = sol1(testArray);
+
+            int sum_test = sol2(testArray);
+            
             Arrays.sort(testArray);
             System.out.println("Sorted: " + Arrays.toString(testArray));
             int sum = 0;
@@ -17,24 +22,27 @@ public class SumWithoutHighestAndLowestNumber {
                 sum += testArray[i];
             System.out.println("The answer is " + sum);
             System.out.println("My method gave " + sum_test);
-
         }
 
         public static int sol1 (int[] nums){
             if (nums == null || nums.length <= 1) return 0;
-
             int min, max, sum;
             min = max = sum = nums[0];
             for (int i = 1; i < nums.length; i++) { 
                 sum += nums[i];
                 System.out.println("sum is " + sum);
-                if (nums[i] < min) {
-                    min = nums[i];
-                } else if (nums[i] > max) {
-                    max = nums[i];
-                } 
+                if (nums[i] < min) min = nums[i];
+                if (nums[i] > max) max = nums[i];
             }
             System.out.println("max is " + max + " min is " + min + " sum before is " + sum);
             return sum - min - max;
+        }
+
+        public static int sol2(int[] nums){
+            if (nums == null || nums.length <= 2)
+                return 0;
+            else {
+                return of(nums).sum();
+            }
         }
 }
